ce91e20f7245fefa58298f1b68114296135af439
Rafael Lopez <lopez.rafael08@gmail.com>
2021-08-31 19:52:29 -0400

    Reactive Beer Service: Assignemnt 4 - Update remaining GET operations to reactive types

--- reactive-brewery-service/src/test/java/com/lopez/rafael/reactivebreweryservice/web/controller/BeerControllerTest.java
+++ reactive-brewery-service/src/test/java/com/lopez/rafael/reactivebreweryservice/web/controller/BeerControllerTest.java
@@ -3,14 +3,18 @@ package com.lopez.rafael.reactivebreweryservice.web.controller;
 import com.lopez.rafael.reactivebreweryservice.bootstrap.BeerLoader;
 import com.lopez.rafael.reactivebreweryservice.services.BeerService;
 import com.lopez.rafael.reactivebreweryservice.web.model.BeerDto;
+import com.lopez.rafael.reactivebreweryservice.web.model.BeerPagedList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -49,5 +53,34 @@ class BeerControllerTest {
                 .expectStatus().isOk()
                 .expectBody(BeerDto.class)
                 .value(beerDto -> beerDto.getBeerName(), equalTo(validBeer.getBeerName()));
+    }
+
+    @Test
+    void getBeerByUpc() {
+        given(beerService.getByUpc(any())).willReturn(validBeer);
+
+        webTestClient.get()
+                .uri("/api/v1/beerUpc/" + validBeer.getUpc())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(BeerDto.class)
+                .value(beerDto -> beerDto.getBeerName(), equalTo(validBeer.getBeerName()));
+    }
+
+    @Test
+    void listBeers() {
+        List<BeerDto> beerList = Arrays.asList(validBeer);
+        BeerPagedList beerPagedList = new BeerPagedList(beerList, PageRequest.of(1, 1), beerList.size());
+
+        given(beerService.listBeers(any(), any(), any(), any())).willReturn(beerPagedList);
+
+        webTestClient.get()
+                .uri("/api/v1/beer")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(BeerPagedList.class)
+                .value(beerDtos -> beerDtos.getContent().size(), equalTo(beerList.size()));
     }
 }
