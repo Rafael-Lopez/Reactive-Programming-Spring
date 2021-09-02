370992661d2b136dd7fa6e0393de01b0a5c6ec1b
Rafael Lopez <lopez.rafael08@gmail.com>
2021-09-02 18:42:25 -0400

    Reactive Beer Service: Refactor List Beers

--- reactive-brewery-service/src/main/java/com/lopez/rafael/reactivebreweryservice/repositories/BeerRepository.java
+++ reactive-brewery-service/src/main/java/com/lopez/rafael/reactivebreweryservice/repositories/BeerRepository.java
@@ -6,11 +6,10 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
 public interface BeerRepository extends ReactiveCrudRepository<Beer, Integer> {
-//    Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
-//
-//    Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
-//
-//    Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, Pageable pageable);
+    // These do not work, but may in the future
+//    Flux<Page<Beer>> findAllByBeerName(String beerName, Pageable pageable);
+//    Flux<Page<Beer>> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
+//    Flux<Page<Beer>> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, Pageable pageable);
 
     Mono<Beer> findByUpc(String upc);
 }
