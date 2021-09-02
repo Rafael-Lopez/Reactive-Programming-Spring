b9f3db0c387952789cec96c96bcbc16074b633fd
Rafael Lopez <lopez.rafael08@gmail.com>
2021-09-02 19:06:10 -0400

    Reactive Beer Service: Refactor Update Beer

--- reactive-brewery-service/src/main/java/com/lopez/rafael/reactivebreweryservice/services/BeerService.java
+++ reactive-brewery-service/src/main/java/com/lopez/rafael/reactivebreweryservice/services/BeerService.java
@@ -6,8 +6,6 @@ import com.lopez.rafael.reactivebreweryservice.web.model.BeerStyleEnum;
 import org.springframework.data.domain.PageRequest;
 import reactor.core.publisher.Mono;
 
-import java.util.UUID;
-
 /**
  * Created by jt on 2019-04-20.
  */
@@ -15,7 +13,7 @@ public interface BeerService {
     Mono<BeerPagedList> listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand);
     Mono<BeerDto> getById(Integer beerId, Boolean showInventoryOnHand);
     Mono<BeerDto> saveNewBeer(BeerDto beerDto);
-    BeerDto updateBeer(UUID beerId, BeerDto beerDto);
+    Mono<BeerDto> updateBeer(Integer beerId, BeerDto beerDto);
     Mono<BeerDto> getByUpc(String upc);
     void deleteBeerById(Integer beerId);
 }
