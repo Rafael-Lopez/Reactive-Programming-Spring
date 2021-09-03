47feb7af0340212fa40a4b3ba8da2943f37ac5d8
Rafael Lopez <lopez.rafael08@gmail.com>
2021-09-02 21:09:04 -0400

    Reactive Beer Service: Functional Endpoints with Spring Flux - Validate SaveNewBeer

--- reactive-brewery-service/src/main/java/com/lopez/rafael/reactivebreweryservice/web/functional/BeerHandlerV2.java
+++ reactive-brewery-service/src/main/java/com/lopez/rafael/reactivebreweryservice/web/functional/BeerHandlerV2.java
@@ -5,8 +5,12 @@ import com.lopez.rafael.reactivebreweryservice.web.model.BeerDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ServerWebInputException;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -14,6 +18,8 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class BeerHandlerV2 {
     private final BeerService beerService;
+    // Provided by Spring Boot by default
+    private final Validator validator;
 
     public Mono<ServerResponse> getBeerById(ServerRequest request) {
         Integer beerId = Integer.valueOf(request.pathVariable("beerId"));
@@ -35,7 +41,8 @@ public class BeerHandlerV2 {
     }
 
     public Mono<ServerResponse> saveNewBeer(ServerRequest request) {
-        Mono<BeerDto> beerDtoMono = request.bodyToMono(BeerDto.class);
+        Mono<BeerDto> beerDtoMono = request.bodyToMono(BeerDto.class)
+                .doOnNext(this::validate);
 
         return beerService.saveNewBeerMono(beerDtoMono)
                 .flatMap(beerDto -> {
@@ -43,5 +50,17 @@ public class BeerHandlerV2 {
                             .header("location", BeerRouterConfiguration.BEER_V2_URL + "/" + beerDto.getId())
                             .build();
                 });
+    }
+
+    // In spring MVC, validation is very simple. We just need to add @Validated (line 83 in BeerController).
+    // At this point in time, Webflux doesn't have anything like that. We actually need to wire that
+    // in a little more manually.
+    private void validate(BeerDto beerDto) {
+        Errors errors = new BeanPropertyBindingResult(beerDto, "beerDto");
+        validator.validate(beerDto, errors);
+
+        if(errors.hasErrors()) {
+            throw new ServerWebInputException(errors.toString());
+        }
     }
 }
