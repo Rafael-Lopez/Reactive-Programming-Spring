292a2f1562721c2118596b544a4110057b8b6daf
Rafael Lopez <lopez.rafael08@gmail.com>
2021-09-05 12:14:01 -0400

    Streaming Stock Quote Service: Add initial project

---
+++ streaming-stock-quote-service/src/main/java/com/lopez/rafael/streamingstockquoteservice/StreamingStockQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lopez.rafael.streamingstockquoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingStockQuoteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingStockQuoteServiceApplication.class, args);
+    }
+
+}
