c422ddbedc6d5d33ef0f75609d94753958ff1b0e
Rafael Lopez <lopez.rafael08@gmail.com>
2021-09-05 13:22:59 -0400

    Reactive Mongo Stock Quote Service: Add initial project

---
+++ reactive-mongo-stock-quote-service/README.md
@@ -0,0 +1,6 @@
+# Mongo Stock Quote Service
+
+#### Useful Commands:
+Run MongoDB in a docker container and expose port 27017
+
+```docker run -d --name mongo-db -p 27017:27017 mongo```
---
+++ reactive-mongo-stock-quote-service/src/main/java/com/lopez/rafael/reactivemongostockquoteservice/ReactiveMongoStockQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lopez.rafael.reactivemongostockquoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMongoStockQuoteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveMongoStockQuoteServiceApplication.class, args);
+    }
+
+}
